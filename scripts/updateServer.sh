ee27837d40b8186990a2869bde9a799d709abf86
Jon Riehl <jriehl@spaceship.com>
2013-10-25 16:12:21 -0500

    Modified updateServer.sh to use EC2 metadata for the new server name if not given from the command line.

--- scripts/updateServer.sh
+++ scripts/updateServer.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 SERVERNAME_PREV="~pipeline/.servername.txt"
+EC2_MAGIC_IP="169.254.169.254"
+EC2_HOSTNAME_URL="http://${EC2_MAGIC_IP}/latest/meta-data/public-hostname"
 
-if [ $# -eq 1 ]; then
-	if [ -f ${SERVERNAME_PREV} ]; then
-		OldServer=`cat ${SERVERNAME_PREV}`;
-	fi
+# First, figure out what the old server name is...
+
+if [ $# -eq 2 ]; then
+    OldServer=$1
+elif [ -f ${SERVERNAME_PREV} ]; then
+    OldServer=$(cat ${SERVERNAME_PREV})
+else
+    echo "Unable to determine old server name."
+    exit 1
+fi
+
+# Then, figure out what the new server name is...
+
+if [ $# -eq 0 ]; then
+    NewServer=$(GET ${EC2_HOSTNAME_URL}) || {
+        echo "Unable to determine EC2 server name (not on an EC2 instance?).";
+        exit 1;
+    }
+elif [ $# -eq 1 ]; then
 	NewServer=$1;
 elif [ $# -eq 2 ]; then
-	OldServer=$1;
 	NewServer=$2;
 else 
-	echo "Usage: $0 [OldServer] NewServer"
+	echo "Usage: $0 [[OldServer] NewServer]"
 	exit 1;
 fi
 
@@ -29,3 +45,4 @@ fi
 ${PREFIX} perl -pi -w -e "s/${OldServer}/${NewServer}/g;" /usr/pipeline/preferences.xml
 
 ${PREFIX} perl -pi -w -e "s/${OldServer}/${NewServer}/g;" /usr/pipeline/serverLibrary/*/*/*.pipe
+FOO=$(GET http://169.254.169.254/latest/meta-data/)
