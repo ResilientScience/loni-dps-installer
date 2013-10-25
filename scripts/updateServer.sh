24494e0bcff020edd05fa09d0a7d7dbae0a6b2b2
Jon Riehl <jriehl@spaceship.com>
2013-10-25 16:22:37 -0500

    Added comments and cannonical expansion of servername file path.

--- scripts/updateServer.sh
+++ scripts/updateServer.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-SERVERNAME_PREV="~pipeline/.servername.txt"
+SERVERNAME_PATH="$(readlink -f ~pipeline)/.servername.txt"
 EC2_MAGIC_IP="169.254.169.254"
 EC2_HOSTNAME_URL="http://${EC2_MAGIC_IP}/latest/meta-data/public-hostname"
 
+# ______________________________________________________________________
 # First, figure out what the old server name is...
 
 if [ $# -eq 2 ]; then
     OldServer=$1
-elif [ -f ${SERVERNAME_PREV} ]; then
-    OldServer=$(cat ${SERVERNAME_PREV})
+elif [ -f ${SERVERNAME_PATH} ]; then
+    OldServer=$(cat ${SERVERNAME_PATH})
 else
     echo "Unable to determine old server name."
     exit 1
 fi
 
+# ______________________________________________________________________
 # Then, figure out what the new server name is...
 
 if [ $# -eq 0 ]; then
@@ -31,7 +33,10 @@ else
 	exit 1;
 fi
 
-echo "${NewServer}" > ~pipeline/.servername.txt
+# ______________________________________________________________________
+# Finally, go to town...
+
+echo "${NewServer}" > ${SERVERNAME_PATH}
 
 echo "Update Pipeline server hostname to ${NewServer}"
 
